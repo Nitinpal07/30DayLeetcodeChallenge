4250e5a0f6ac9f4a3c2f1e376ab6714e5d99a05d
Nitinpal07 <32491448+Nitinpal07@users.noreply.github.com>
2020-04-30 15:04:08 +0530

    Create MinStack.cpp

---
+++ 30-DayLeetCodingChallenge/day10/MinStack.cpp
@@ -0,0 +1,53 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    stack<int> s;
+    stack<int> mins;
+    
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        if(s.size()==0){
+            s.push(x);
+            mins.push(x);
+        }
+        else{
+            if(x>mins.top()){
+                s.push(x);
+            }
+            else{
+               s.push(x);
+               mins.push(x); 
+            }
+        }
+    }
+    
+    void pop() {
+        if(s.top()==mins.top()){
+            s.pop();
+            mins.pop();
+        }
+        else{
+            s.pop();
+        }
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return mins.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
