d061dbfa461fbd12e1be26d04e9025a4f572ab9f
Nitinpal07 <32491448+Nitinpal07@users.noreply.github.com>
2020-05-02 17:19:55 +0530

    Create JewelsandStones.cpp

---
+++ 30daymaychallenge/day2/JewelsandStones.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        map<char,int> m;
+        for(int i=0;i<S.size();i++){
+            m[S[i]]++;
+        }
+        int ans=0;
+        for(int i=0;i<J.size();i++){
+            if(m.find(J[i])!=m.end()){
+                ans+=m[J[i]];
+            }
+        }
+        return ans;
+    }
+};
